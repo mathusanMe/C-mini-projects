b7fd001cac4e13f8bed548ce1150e4f5d1ca1055
mathusanMe <mathusan.selvakumar+github@gmail.com>
2023-11-12 11:54:37 +0100

    Add main

--- tar-utilities/src/main.c
+++ tar-utilities/src/main.c
@@ -1,4 +1,27 @@
-int main() {
-    // Your program logic here
+#include "main.h"
+
+int main(int argc, char *argv[]) {
+    
+    if (argc != 3) {
+        puts("Usage: program `command` `tarname`");
+        puts("Commands: l : list-tar, d : de-tar, m : mk-tar");
+        return -1;
+    }
+
+    char command = argv[1][0];
+
+    switch (command) {
+        case 'l':
+            return listar(argv[2]);
+        case 'd':
+            break;
+        case 'm':
+            break;
+        default:
+            puts("Usage: program `command` `tarname`");
+            puts("Commands: list-tar, de-tar, mk-tar");
+            return -1;
+    }
+
     return 0;
 }
---
+++ tar-utilities/src/main.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include "commands/list-tar/list-tar.h"
+
+int main(int argc, char *argv[]);
